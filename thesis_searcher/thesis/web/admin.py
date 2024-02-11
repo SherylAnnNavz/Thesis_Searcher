f4563c04a355a61086fa9793492de27217cadd73
SherylAnnNavz <sherylnavarro29@gmail.com>
2024-02-11 16:09:10 +0800

    Added some changes

--- thesis_searcher/thesis/web/admin.py
+++ thesis_searcher/thesis/web/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','publication_date')
+    list_filter = ("status",)
+    search_fields = ['title', 'abstract']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
