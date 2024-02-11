f985163cc0c7414a1e2c9ee3f0c46acd5b041e1e
SherylAnnNavz <sherylnavarro29@gmail.com>
2024-02-11 20:43:56 +0800

    Made some changes

--- thesis_searcher/thesis/web/urls.py
+++ thesis_searcher/thesis/web/urls.py
@@ -1,9 +1,9 @@
 # thesis_searcher/thesis/web/urls.py
 
 from django.urls import path
-from . import views  # Import views from the same directory
+from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),  # Assuming post_list is the view function in views.py
+    path('', views.myfirst, name='myfirst'),
     # Add other URL patterns as needed
 ]
