c2c5b08ec0ff01a4d54d9cfed503605d74cc9d2c
SherylAnnNavz <sherylnavarro29@gmail.com>
2024-02-11 20:13:55 +0800

    Make some changes

--- thesis_searcher/thesis/web/urls.py
+++ thesis_searcher/thesis/web/urls.py
@@ -1,7 +1,7 @@
-# thesis/web/urls.py
+# thesis_searcher/thesis/web/urls.py
 
 from django.urls import path
-from . import views  # Import views from the web app
+from . import views  # Import views from the same directory
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),  # Assuming post_list is the view function in views.py
--- thesis_searcher/thesis/web/views.py
+++ thesis_searcher/thesis/web/views.py
@@ -1,7 +1,12 @@
-# views.py
-from django.shortcuts import render
+# thesis_searcher/thesis/web/views.py
+
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(status=1).order_by('-publication_date')
     return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
