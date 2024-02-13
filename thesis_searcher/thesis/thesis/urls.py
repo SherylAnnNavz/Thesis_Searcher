de11a115f09cae03dc553064f14e2c15edc98892
SherylAnnNavz <sherylnavarro29@gmail.com>
2024-02-13 17:09:09 +0800

    Added some changes

--- thesis_searcher/thesis/thesis/urls.py
+++ thesis_searcher/thesis/thesis/urls.py
@@ -5,5 +5,6 @@ from web.views import thesis_list, thesis_detail  # Import your views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', thesis_list, name="homepage"),  # Use thesis_list view for the homepage
+    path('thesis/', thesis_list, name="thesis_list"),  # Add a URL pattern for the thesis list view
     path('thesis/<int:thesis_id>/', thesis_detail, name="post_detail"),  # Route for thesis detail view
 ]
--- thesis_searcher/thesis/web/views.py
+++ thesis_searcher/thesis/web/views.py
@@ -3,8 +3,8 @@ from .models import Thesis
 
 def thesis_list(request):
     theses = Thesis.objects.all()
-    return render(request, 'post_list.html', {'web': theses})
+    return render(request, 'post_list.html', {'theses': theses})  # Update context variable name to 'theses'
 
 def thesis_detail(request, thesis_id):
     thesis = Thesis.objects.get(id=thesis_id)
-    return render(request, 'post_detail.html', {'web': thesis})
+    return render(request, 'post_detail.html', {'thesis': thesis})  # Keep the context variable name as 'thesis'
