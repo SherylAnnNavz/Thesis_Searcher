ba4d762a9c7db858e23f97a5b2c2f2ad75e1a1ed
SherylAnnNavz <sherylnavarro29@gmail.com>
2024-02-13 09:46:15 +0800

    Added some changes

--- thesis_searcher/thesis/web/views.py
+++ thesis_searcher/thesis/web/views.py
@@ -3,8 +3,8 @@ from .models import Thesis
 
 def thesis_list(request):
     theses = Thesis.objects.all()
-    return render(request, 'templates/post_list.html', {'web': theses})
+    return render(request, 'post_list.html', {'web': theses})
 
 def thesis_detail(request, thesis_id):
     thesis = Thesis.objects.get(id=thesis_id)
-    return render(request, 'templates/post_detail.html', {'web': thesis})
+    return render(request, 'post_detail.html', {'web': thesis})
