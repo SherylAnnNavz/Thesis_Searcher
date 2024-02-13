aa80b7380b411851d6a079a0dc45e98687cf226a
SherylAnnNavz <sherylnavarro29@gmail.com>
2024-02-13 16:45:05 +0800

    Made some changes

--- thesis_searcher/thesis/thesis/urls.py
+++ thesis_searcher/thesis/thesis/urls.py
@@ -5,5 +5,5 @@ from web.views import thesis_list, thesis_detail  # Import your views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', thesis_list, name="homepage"),  # Use thesis_list view for the homepage
-    path('thesis/<int:thesis_id>/', thesis_detail, name="thesis_detail"),  # Route for thesis detail view
+    path('thesis/<int:thesis_id>/', thesis_detail, name="post_detail"),  # Route for thesis detail view
 ]
