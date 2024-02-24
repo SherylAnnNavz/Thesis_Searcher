1add9977bfd059127bead16cc10677710ae81a85
SherylAnnNavz <sherylnavarro29@gmail.com>
2024-02-24 11:51:56 +0800

    Added search function

--- thesis_searcher/thesis/thesis/urls.py
+++ thesis_searcher/thesis/thesis/urls.py
@@ -1,10 +1,13 @@
+# urls.py
+
 from django.contrib import admin
-from django.urls import path, include  
-from web.views import thesis_list, thesis_detail  # Import your views
+from django.urls import path
+from web.views import thesis_list, thesis_detail, thesis_search  # Import views directly
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', thesis_list, name="homepage"),  # Use thesis_list view for the homepage
-    path('thesis/', thesis_list, name="thesis_list"),  # Add a URL pattern for the thesis list view
+    path('thesis/', thesis_list, name="post_list"),  # Add a URL pattern for the thesis list view
     path('thesis/<int:thesis_id>/', thesis_detail, name="post_detail"),  # Route for thesis detail view
+    path('search/', thesis_search, name="thesis_search"),  # Route for thesis search view
 ]
--- thesis_searcher/thesis/web/views.py
+++ thesis_searcher/thesis/web/views.py
@@ -1,3 +1,5 @@
+# views.py
+
 from django.shortcuts import render
 from .models import Thesis
 
@@ -8,3 +10,12 @@ def thesis_list(request):
 def thesis_detail(request, thesis_id):
     thesis = Thesis.objects.get(id=thesis_id)
     return render(request, 'post_detail.html', {'thesis': thesis})
+
+def thesis_search(request):
+    query = request.GET.get('q')  # Get the search query from the URL parameters
+    if query:
+        theses = Thesis.objects.filter(title__icontains=query) | Thesis.objects.filter(abstract__icontains=query)
+    else:
+        theses = Thesis.objects.all()  # If no query is provided, return all theses
+
+    return render(request, 'search_results.html', {'theses': theses, 'query': query})
