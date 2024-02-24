ce81cc89243fcbcaf8b6e940ce651e6ce7517817
SherylAnnNavz <sherylnavarro29@gmail.com>
2024-02-24 11:55:31 +0800

    Made changes

--- thesis_searcher/thesis/thesis/urls.py
+++ thesis_searcher/thesis/thesis/urls.py
@@ -7,7 +7,7 @@ from web.views import thesis_list, thesis_detail, thesis_search  # Import views 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', thesis_list, name="homepage"),  # Use thesis_list view for the homepage
-    path('thesis/', thesis_list, name="post_list"),  # Add a URL pattern for the thesis list view
+    path('thesis/', thesis_list, name="thesis_list"),  # Corrected URL pattern name for the thesis list view
     path('thesis/<int:thesis_id>/', thesis_detail, name="post_detail"),  # Route for thesis detail view
     path('search/', thesis_search, name="thesis_search"),  # Route for thesis search view
 ]
